83c37169b9b14c0f0aef885847554f6df5536b46
shiv <shiv.laljat@zopsmart.com>
2022-01-21 14:19:13 +0530

    :::Added::: Movie Detail page UI show Movie Poster using glid Show Movie Details

--- app/src/main/java/com/zopsmart/mymovies/MovieDetailActivity.kt
+++ app/src/main/java/com/zopsmart/mymovies/MovieDetailActivity.kt
@@ -3,26 +3,47 @@ package com.zopsmart.mymovies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.zopsmart.mymovies.ViewModels.MovieDetailViewFactory
 import com.zopsmart.mymovies.ViewModels.MovieDetailViewModel
 import com.zopsmart.mymovies.api.RetrofitHelper
 import com.zopsmart.mymovies.api.apiInterface
 import com.zopsmart.mymovies.repository.MovieDetailRepository
-import com.zopsmart.mymovies.repository.PopularMovieRepository
 
 class MovieDetailActivity : AppCompatActivity() {
 
     lateinit var movieDetailViewModel: MovieDetailViewModel
+    lateinit var movieTitleTextView : TextView
+    lateinit var likeFab : FloatingActionButton
+    lateinit var moviePosterImageView : ImageView
+    lateinit var collapsingToolbar: CollapsingToolbarLayout
+    lateinit var releaseDateTextView : TextView
+    lateinit var ratingBar: RatingBar
+    lateinit var overviewTextView: TextView
 
     var movieId : Int  = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail2)
-
         val movieService = RetrofitHelper.getInstance().create(apiInterface::class.java)
         val repository= MovieDetailRepository(movieService)
+
+
+        moviePosterImageView = findViewById(R.id.moviePosterImageView)
+        collapsingToolbar = findViewById(R.id.collapsingToolBar)
+        releaseDateTextView = findViewById(R.id.releaseDateTextView)
+        movieTitleTextView = findViewById(R.id.movieTitleTextView)
+        ratingBar = findViewById(R.id.ratingBar)
+        overviewTextView = findViewById(R.id.synopsisTextView)
+        likeFab = findViewById(R.id.likeFab)
+
 
 
         if(intent != null)
@@ -36,10 +57,21 @@ class MovieDetailActivity : AppCompatActivity() {
 
         movieDetailViewModel.movieDetailLiveData.observe(this,{
             Log.d("kkk", "movie detail response : $it")
+            collapsingToolbar.title = it.title
+            movieTitleTextView.text = it.title
+            ratingBar.rating = (it.voteAverage / 2).toFloat()
+            releaseDateTextView.text = it.releaseDate
+            overviewTextView.text = it.overview
+
+            Glide
+                .with(this)
+                .load("https://www.themoviedb.org/t/p/w500${it.posterPath}")
+                .fitCenter()
+                .into(moviePosterImageView)
         })
 
-    }
 
+    }
 
 
 }
--- app/src/main/java/com/zopsmart/mymovies/api/Retrofithelper.kt
+++ app/src/main/java/com/zopsmart/mymovies/api/Retrofithelper.kt
@@ -1,6 +1,6 @@
 package com.zopsmart.mymovies.api
 
-import dagger.Provides
+
 import okhttp3.Credentials
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
@@ -9,7 +9,7 @@ import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Singleton
+
 
 object RetrofitHelper {
     private const val BASE_URL = "https://api.themoviedb.org/3/"
